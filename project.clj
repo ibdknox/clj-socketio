55879b1ec90623d235225f13e38d4eef1323ae20
Chris Granger <ibdknox@gmail.com>
2011-04-28 16:34:19 -0700

    intial import
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clj-socketio "0.1.0"
+  :description "A clojure wrapper around the netty-socket-io implementation."
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
+
---
+++ src/clj_socketio/core.clj
@@ -0,0 +1,1 @@
+(ns clj-socketio.core)
