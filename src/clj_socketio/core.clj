d93463baeb5231d9fa5372cba386b470cfd472bb
Chris Granger <ibdknox@gmail.com>
2011-04-30 14:34:45 -0700

    add in the ability to create multiple servers and start/stop them at will
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- src/clj_socketio/core.clj
+++ src/clj_socketio/core.clj
@@ -1,35 +1,29 @@
 (ns clj-socketio.core
   (:import [com.ibdknox.socket_io_netty NSIOServer INSIOHandler INSIOClient SocketIOUtils]))
 
-(def listeners (atom {:connected []
-                      :disconnected []
-                      :received []
-                      :shutdown []}))
+(defonce *servers* (atom {}))
 
-(defn route-event [event-type & args]
-  (doseq [cur-fn (get @listeners event-type)]
+(defn route-event [server event-type & args]
+  (doseq [cur-fn (get-in @*servers* [server event-type])]
     (apply cur-fn args)))
 
-(def *handler* (reify INSIOHandler 
-                 (OnConnect [this client] (route-event :connected client))
-                 (OnDisconnect [this client] (route-event :disconnected client))
-                 (OnMessage [this client msg] (route-event :received client msg))
-                 (OnShutdown [this] (route-event :shutdown))))
+(defn- on [server event func]
+  (swap! *servers* #(update-in % [server event] conj func)))
 
-(defn- on [event func]
-  (swap! listeners #(assoc % event (conj (get % event) func))))
+(defn on-connect [{server :id} func]
+  (on server :connected func))
 
-(defn on-connect [func]
-  (on :connected func))
+(defn on-disconnect [{server :id} func]
+  (on server :disconnected func))
 
-(defn on-disconnect [func]
-  (on :disconnected func))
+(defn on-message [{server :id} func]
+  (on server :received func))
 
-(defn on-message [func]
-  (on :received func))
+(defn on-shutdown [{server :id} func]
+  (on server :shutdown func))
 
-(defn on-shutdown [func]
-  (on :shutdown func))
+(defn session-id [client]
+  (.getSessionID client))
 
 (defn send-to [client msg]
   (if-not (coll? client)
@@ -38,5 +32,19 @@
       (doseq [cl client]
         (.sendUnencoded cl final-msg)))))
 
-(defn start [port]
-  (NSIOServer/start *handler* port))
+(defn create [port]
+  (let [id port
+        handler (reify INSIOHandler 
+                  (OnConnect [this client] (route-event id :connected client))
+                  (OnDisconnect [this client] (route-event id :disconnected client))
+                  (OnMessage [this client msg] (route-event id :received client msg))
+                  (OnShutdown [this] (route-event id :shutdown)))]
+    {:server (NSIOServer. handler port) :port port :id port}))
+
+(defn start [{server :server}]
+  (.start server))
+
+(defn stop [{server :server id :id}]
+  (.stop server)
+  (swap! *servers* #(update-in % [id] (fn [x] nil))))
+
