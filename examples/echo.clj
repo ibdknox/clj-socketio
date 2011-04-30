5d04dc57f3959a4cd7eb7c0382e8502faf06066e
Chris Granger <ibdknox@gmail.com>
2011-04-30 14:43:51 -0700

    fix sample
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- examples/echo.clj
+++ examples/echo.clj
@@ -1,13 +1,16 @@
 (ns clj-socketio.examples.echo
   (:use clj-socketio.core))
 
+(def server (create 8080))
 (def clients (atom #{}))
 
-(on-connected (fn [client]
-                (swap! clients conj client)))
+(on-connected server (fn [client]
+                       (swap! clients conj client)))
 
-(on-disconnect (fn [client]
-                   (swap! clients disj client)))
+(on-disconnect server (fn [client]
+                        (swap! clients disj client)))
 
-(on-message (fn [client msg]
-              (println msg)))
+(on-message server (fn [client msg]
+                     (println msg)))
+
+(start server)
